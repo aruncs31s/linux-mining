b3cf393219e2468dc232ad69b8842a6a06c955bf
aruncs31s <aruncs31ss@gmail.com>
2022-09-03 23:06:00 +0530

    new fastsetup

---
+++ termuxSetup.sh
@@ -0,0 +1,59 @@
+#!/data/data/com.termux/files/usr/bin/bash
+pkgs=("git" "proot" "build-essential" "cmake" )
+printf "updating package lists\n"
+apt update &>/dev/null
+prefix=/usr
+printf "Performing a package upgrade want to continue (y/n)"
+read a 
+ if [[ $a == "y" ]]
+    then
+        printf "This will take time :)\n"
+        apt upgrade -y &>/dev/null
+    fi
+requirements(){
+    for str in ${pkgs[@]}; do
+    printf "\ninstalling $str" 
+    apt install $str -y  &>/dev/null
+    done
+}
+main(){
+      check(){
+        if [[(-f $prefix/bin/$str)]]; then
+        printf "\n  $str is found"
+        else 
+        printf "\n  $str not found !"
+        fi
+        }
+    for str in ${pkgs[@]}; do
+    check
+    done
+    printf "\n Do you want to install the missing packages (y/n) "
+    read ans
+    if [[ $ans == "y" ]]
+    then 
+    requirements
+    fi
+    clonning
+}
+clonning (){
+    printf "\n Cloning xmrig repo"
+    git clone https://github.com/xmrig/xmrig &>/dev/null
+    printf "\n xmrig directry will be at $pwd"
+    cd xmrig 
+    printf "\n xmrig/build directry will be at $pwd"
+    mkdir build
+
+    cd build
+
+    cmake -DWITH_HWLOC=OFF .. 
+    make
+
+
+}
+afterInstall(){
+    printf "\n xmrig is now installed "
+}
+main
+afterInstall
+
+
